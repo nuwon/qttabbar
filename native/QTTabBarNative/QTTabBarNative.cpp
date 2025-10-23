50f4d6f552bfb3ae1e25ea7b8cd70852bdb43549
shefben <shefben@gmail.com>
2025-10-23 16:57:31 -0500

    Add native AutoLoader COM support

--- native/QTTabBarNative/QTTabBarNative.cpp
+++ native/QTTabBarNative/QTTabBarNative.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
+#include "AutoLoaderNative.h"
 #include "QTTabBarClass.h"
 
 using namespace ATL;
 
 OBJECT_ENTRY_AUTO(__uuidof(QTTabBarClass), QTTabBarClass)
+OBJECT_ENTRY_AUTO(__uuidof(AutoLoaderNative), AutoLoaderNative)
